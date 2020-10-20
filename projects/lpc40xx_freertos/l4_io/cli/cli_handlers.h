9f8ac3770042e9a1f469a4b88377ff0acaf34352
Tejas Pidkalwar <tejas.pidkalwar@gmail.com>
2020-10-19 19:39:36 -0700

    Added 2 tasks to play around in FreeRTOS simulator, Added CLI command handler for taskcontrol command

--- projects/lpc40xx_freertos/l4_io/cli/cli_handlers.h
+++ projects/lpc40xx_freertos/l4_io/cli/cli_handlers.h
@@ -11,3 +11,10 @@ app_cli_status_e cli__i2c(app_cli__argument_t argument, sl_string_t user_input_m
 
 app_cli_status_e cli__task_list(app_cli__argument_t argument, sl_string_t user_input_minus_command_name,
                                 app_cli__print_string_function cli_output);
+
+#ifdef PROD_CONS_ASSIGNMENT
+
+app_cli_status_e cli__task_control(app_cli__argument_t argument, sl_string_t user_input_minus_command_name,
+                                   app_cli__print_string_function cli_output);
+
+#endif
--- projects/x86_freertos/main.c
+++ projects/x86_freertos/main.c
@@ -1,10 +1,41 @@
 #include <stdio.h>
 
 #include "FreeRTOS.h"
+#include "semphr.h"
 #include "task.h"
+#include "assignment_include.h"
 
 #include "cpu_utilization_task.h"
 
+typedef enum { switch__off, switch__on } switch_e;
+
+#ifdef PROD_CONS_ASSIGNMENT
+static QueueHandle_t prod_to_cons_queue;
+
+static switch_e get_switch_input_from_switch(void) { return switch__on; }
+
+void producer_task(void *p) {
+  while (1) {
+    const switch_e switch_value = get_switch_input_from_switch();
+    printf("Ping from Before sending the msg\n");
+    if (!xQueueSend(prod_to_cons_queue, &switch_value, 0)) {
+      fprintf(stderr, "Unable to send data over RTOS queue");
+    }
+    printf("Ping after sending %d data over the RTOS queue\n", switch_value);
+    vTaskDelay(1000);
+  }
+}
+
+void consumer_task(void *p) {
+  switch_e switch_value = switch__off;
+  while (1) {
+    if (xQueueReceive(prod_to_cons_queue, &switch_value, 0)) {
+      printf("The data %d is received over the RTOS queue\n", switch_value);
+    }
+  }
+}
+
+#endif
 /**
  * This POSIX based FreeRTOS simulator is based on:
  * https://github.com/linvis/FreeRTOS-Sim
@@ -14,8 +45,13 @@
  * This is a great teaching tool though :)
  */
 int main(void) {
+#ifdef PROD_CONS_ASSIGNMENT
+  prod_to_cons_queue = xQueueCreate(10, sizeof(switch_e));
+  xTaskCreate(producer_task, "Producer", 2048 / sizeof(void *), NULL, PRIORITY_LOW, NULL);
+  xTaskCreate(consumer_task, "Consumer", 2048 / sizeof(void *), NULL, PRIORITY_LOW, NULL);
+#else
   xTaskCreate(cpu_utilization_print_task, "cpu", 1, NULL, PRIORITY_LOW, NULL);
-
+#endif
   puts("Starting FreeRTOS Scheduler ..... \r\n");
   vTaskStartScheduler();
 
