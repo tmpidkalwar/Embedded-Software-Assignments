0d19ff9c3a7dbeb8e01d781e5ff6f1083921d72b
Tejas Pidkalwar <tejas.pidkalwar@gmail.com>
2020-10-24 17:23:02 -0700

    assignment include file is added

---
+++ projects/lpc40xx_freertos/assignment_include.h
@@ -0,0 +1,18 @@
+/**
+ * @file assignment_include.h
+ * @author Tejas Pidkalwar
+ * @date 19th Sept 2020
+ * @brief This file maintains the macro defined to enable specific assignment implementation for
+ *      execution, along with subparts of assignment enabling feature
+ */
+#ifndef _ASSIGNMENT_INCLUDE_
+#define _ASSIGNMENT_INCLUDE_
+
+/*
+ * This file defines macros corresponding each assignment.
+ * Enable only one assignment macro while compiling.
+ * This file also defines macros for sub parts corresponding to each assignment.
+ * Take care to enable only one part macro enabled while compiling.
+ */
+
+#endif 
